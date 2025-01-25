f248b176181c13f4f55f2cde694716a89e173026
Divya <divya.inapakurthi@gmail.com>
2025-01-25 14:30:59 -0600

    folder delete added

--- src/components/Folderitem.jsx
+++ src/components/Folderitem.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-function Folderitem({ folder }) {
+
+import db from "../firebase_config"
+import { deleteDoc, collection, doc } from "firebase/firestore";
+function Folderitem({ folder, getFolders }) {
     const date = folder.date.split(" ");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const folderdbRef = collection(db, "folders");
+        const folderRef =doc(folderdbRef,folder.id)
+        await deleteDoc(folderRef)
+        getFolders();
+    }
+
     return (
         <>
 
             <Link className='folder__item' to={`/notes/${folder.id}`}>
                 <div className="folder__content">
-                    <h3 className='folder__title'>{folder.title}</h3>
-                    <p className='folder__date'>{date[1] + " " + date[2]} </p>
+                    <div className="content">
+                        <h3 className='folder__title'>{folder.title}</h3>
+                        <p className='folder__date'>{date[1] + " " + date[2]} </p>
+                    </div>
+                    <div className="delete__icon">
+                        <button onClick={handleSubmit}>delete</button>
+                    </div>
                 </div>
+
             </Link>
 
         </>
